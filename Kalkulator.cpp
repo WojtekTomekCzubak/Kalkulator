12ba8e918c203cedaa43749d17d8e5d557468441
WojtekTomekCzubak <wojtek.czubakk@gmail.com>
2024-03-13 18:51:39 +0100

    Dodanie instrukcji petli while i deklaracji funkcji

--- Kalkulator.cpp
+++ Kalkulator.cpp
@@ -4,4 +4,8 @@ int main()
 {
 	//Stworz petle while ktora wykonuje sie nieskonczenie dlugo
 	//Zadeklaruj funkcje "dodawanie" "odejmowanie" "mnozenie" "dzielenie"
+
+	//stworz w odpowiednim miejscu zmienna liczbowa przechowujaca decyzje uzytkownika 
+	//w petli while wypisz mozliwe opcje tj. od 1 do 4 z odpowiednimi nazwami funkcji
 }
+ 
